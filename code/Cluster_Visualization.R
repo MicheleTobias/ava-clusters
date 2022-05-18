117d9cabf3ffccaf180f11b94035c4beea6059d0
Michele Tobias <mmtobias@ucdavis.edu>
2022-05-17 20:01:13 -0700

    Add link to blog

--- code/Cluster_Visualization.R
+++ code/Cluster_Visualization.R
@@ -1,5 +1,7 @@
 #GOAL: visualization of the cluster data
 
+# helpful post: http://www.sthda.com/english/wiki/beautiful-dendrogram-visualizations-in-r-5-must-known-methods-unsupervised-machine-learning 
+
 
 # Set Up ------------------------------------------------------------------
 
